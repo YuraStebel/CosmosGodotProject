d5976fc9a0c5f39ee8477f7cb0cc0ef341199264
YuraStebel <steblevskiy.yu.ve@yandex.ru>
2025-08-31 23:16:56 +0300

    giga patch

--- rigid_body_3d.gd
+++ rigid_body_3d.gd
@@ -16,6 +16,9 @@ func _unhandled_input(event: InputEvent) -> void:
 		apply_torque(transform.basis * Vector3(-event.relative.y, -event.relative.x, 0.0))
 		print(event.relative)
 		print("))")
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
 
 
 
@@ -23,7 +26,10 @@ func _unhandled_input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	var horizontal_dir := Input.get_vector("left", "righ", "forward", "backward")
 	var vertical_dir := Input.get_axis("down", "up")
+	var roll_dir := Input.get_axis("lean_left", "lean_right")
 	#print(horizontal_dir)
 	apply_central_force(transform.basis * Vector3(horizontal_dir.x, vertical_dir, horizontal_dir.y) * move_force)
+	
+	apply_torque(Vector3(0.0, 0.0, -roll_dir * move_force))
 	
 	#print(angular_velocity)
