7d80ad58bb40457d00af65545a0b65ea0ffba2b1
mnb <fewewe96@icloud.com>
2025-09-01 23:00:48 +0300

    added oxygen bar

--- scripts/stats.gd
+++ scripts/stats.gd
@@ -1,13 +1,14 @@
 extends Node
 
+#КИСЛОРОД
 var max_oxygen: float = 100
 var current_oxygen: float
 var oxygen_decrease_rate: float = 1.0
 var oxygen_reloading_rate: float = 10.0
-
-
 var is_in_oxygen_zone = false
 
+#HUD
+@onready var oxygen_bar = $"../Control/HUD/OxygenBar"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	current_oxygen = max_oxygen
@@ -15,8 +16,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	if not is_multiplayer_authority():
+		return
+		
 	if is_in_oxygen_zone:
 		current_oxygen = min(current_oxygen + oxygen_reloading_rate * delta, max_oxygen)
 	else:
 		current_oxygen = max(current_oxygen - oxygen_decrease_rate * delta, 0)
 	print(current_oxygen)
+	
+	oxygen_bar.value = current_oxygen
