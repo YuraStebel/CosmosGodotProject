7a65606112aed65000c7e93cde5000c7bf1f3054
mnb <fewewe96@icloud.com>
2025-09-01 23:52:28 +0300

    added healthbar

--- scripts/rigid_body_3d.gd
+++ scripts/rigid_body_3d.gd
@@ -20,15 +20,16 @@ func _ready() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if not is_multiplayer_authority(): return
 	
-	if event is InputEventMouseMotion:
-		var local_torque = Vector3(
-			-event.relative.y * mouse_sens,
-			-event.relative.x * mouse_sens,
-			0
-		)
-		
-		var world_torque = transform.basis * local_torque
-		apply_torque(world_torque)
+	if stats.is_alive:
+		if event is InputEventMouseMotion:
+			var local_torque = Vector3(
+				-event.relative.y * mouse_sens,
+				-event.relative.x * mouse_sens,
+				0
+			)
+			
+			var world_torque = transform.basis * local_torque
+			apply_torque(world_torque)
 
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
@@ -37,13 +38,14 @@ func _unhandled_input(event: InputEvent) -> void:
 func _physics_process(_delta: float) -> void:
 	if not is_multiplayer_authority(): return
 	
-	var horizontal_dir := Input.get_vector("left", "righ", "forward", "backward")
-	var vertical_dir := Input.get_axis("down", "up")
-	var roll_dir := Input.get_axis("lean_left", "lean_right")
-	#print(horizontal_dir)
-	apply_central_force(transform.basis * Vector3(horizontal_dir.x, vertical_dir, horizontal_dir.y) * move_force)
-	
-	apply_torque(transform.basis * Vector3(0.0, 0.0, -roll_dir * roll_force))
+	if stats.is_alive:
+		var horizontal_dir := Input.get_vector("left", "righ", "forward", "backward")
+		var vertical_dir := Input.get_axis("down", "up")
+		var roll_dir := Input.get_axis("lean_left", "lean_right")
+		#print(horizontal_dir)
+		apply_central_force(transform.basis * Vector3(horizontal_dir.x, vertical_dir, horizontal_dir.y) * move_force)
+		
+		apply_torque(transform.basis * Vector3(0.0, 0.0, -roll_dir * roll_force))
 	
 	
 	#print(angular_velocity)
--- scripts/stats.gd
+++ scripts/stats.gd
@@ -1,15 +1,21 @@
 extends Node
 
 #КИСЛОРОД
-var max_oxygen: float = 100
+var max_oxygen: float = 5
 var current_oxygen: float
 var oxygen_decrease_rate: float = 1.0
 var oxygen_reloading_rate: float = 10.0
 var is_in_oxygen_zone = false
 
+#ЗДОРОВЬЕ
+var max_health: float = 100.0
+var current_health: float
+var health_decrease_by_oxygen_rate: float = 10.0
+var is_alive = true
 #HUD
-@onready var control_node: Control = $"../Control"
-@onready var oxygen_bar = $"../Control/HUD/OxygenBar"
+@onready var control_node: Control = $"../HUD"
+@onready var oxygen_bar = $"../HUD/VBoxContainer/OxygenBar"
+@onready var health_bar = $"../HUD/VBoxContainer2/HealthBar"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if not is_multiplayer_authority():
@@ -17,6 +23,7 @@ func _ready() -> void:
 	
 	control_node.show()
 	current_oxygen = max_oxygen
+	current_health = max_health
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,5 +40,17 @@ func _physics_process(delta: float) -> void:
 		oxygen_bar.visible = false
 	else:
 		oxygen_bar.visible = true
+	if current_health == max_health:
+		health_bar.visible = false
+	else:
+		health_bar.visible = true
 	
+	if current_oxygen <= 0:
+		current_health = max(current_health - health_decrease_by_oxygen_rate * delta, 0)
+	
+	if current_health == 0:
+		is_alive = false
+	
+	print(current_health)
 	oxygen_bar.value = current_oxygen
+	health_bar.value = current_health
